65ac50070bcdd3e7325a2d640650825c58adf4bd
Thomas Honeyman <admin@thomashoneyman.com>
2020-12-09 12:40:22 -0800

    Migrate to GitHub Actions (#24)

--- src/Node/ChildProcess.js
+++ src/Node/ChildProcess.js
@@ -1,131 +1,139 @@
-'use strict';
+"use strict";
 
 /* eslint-env node*/
 
-exports.unsafeFromNullable = function unsafeFromNullable (msg) {
-    return function (x) {
-        if (x === null) throw new Error(msg);
-        return x;
-    };
+exports.unsafeFromNullable = function unsafeFromNullable(msg) {
+  return function (x) {
+    if (x === null) throw new Error(msg);
+    return x;
+  };
 };
 
-exports.spawnImpl = function spawnImpl (command) {
-    return function (args) {
-        return function (opts) {
-            return function () {
-                return require('child_process').spawn(command, args, opts);
-            };
-        };
-    };
-};
-
-exports.execImpl = function execImpl (command) {
+exports.spawnImpl = function spawnImpl(command) {
+  return function (args) {
     return function (opts) {
-        return function (callback) {
-            return function () {
-                return require('child_process').exec(command, opts, function (err, stdout, stderr) {
-                    callback(err)(stdout)(stderr)();
-                });
-            };
-        };
+      return function () {
+        return require("child_process").spawn(command, args, opts);
+      };
     };
+  };
 };
 
-
-exports.execFileImpl = function execImpl (command) {
-    return function (args) {
-        return function (opts) {
-            return function (callback) {
-                return function () {
-                    return require('child_process').execFile(command, args, opts, function (err, stdout, stderr) {
-                        callback(err)(stdout)(stderr)();
-                    });
-                };
-            };
-        };
+exports.execImpl = function execImpl(command) {
+  return function (opts) {
+    return function (callback) {
+      return function () {
+        return require("child_process").exec(
+          command,
+          opts,
+          function (err, stdout, stderr) {
+            callback(err)(stdout)(stderr)();
+          }
+        );
+      };
     };
+  };
 };
 
-exports.execSyncImpl = function execSyncImpl (command) {
+exports.execFileImpl = function execImpl(command) {
+  return function (args) {
     return function (opts) {
+      return function (callback) {
         return function () {
-            return require('child_process').execSync(command, opts);
+          return require("child_process").execFile(
+            command,
+            args,
+            opts,
+            function (err, stdout, stderr) {
+              callback(err)(stdout)(stderr)();
+            }
+          );
         };
+      };
     };
+  };
 };
 
-exports.execFileSyncImpl = function execFileSyncImpl (command) {
-    return function (args) {
-        return function (opts) {
-            return function () {
-                return require('child_process').execFileSync(command, args, opts);
-            };
-        };
+exports.execSyncImpl = function execSyncImpl(command) {
+  return function (opts) {
+    return function () {
+      return require("child_process").execSync(command, opts);
     };
+  };
 };
 
-exports.fork = function fork (cmd) {
-    return function (args) {
-        return function () {
-            return require('child_process').fork(cmd, args);
-        };
+exports.execFileSyncImpl = function execFileSyncImpl(command) {
+  return function (args) {
+    return function (opts) {
+      return function () {
+        return require("child_process").execFileSync(command, args, opts);
+      };
     };
+  };
 };
 
-exports.mkOnExit = function mkOnExit (mkChildExit) {
-    return function onExit (cp) {
-        return function (cb) {
-            return function () {
-                cp.on('exit', function (code, signal) {
-                    cb(mkChildExit(code)(signal))();
-                });
-            };
-        };
+exports.fork = function fork(cmd) {
+  return function (args) {
+    return function () {
+      return require("child_process").fork(cmd, args);
     };
+  };
 };
 
-exports.mkOnClose = function mkOnClose (mkChildExit) {
-    return function onClose (cp) {
-        return function (cb) {
-            return function () {
-                cp.on('close', function (code, signal) {
-                    cb(mkChildExit(code)(signal))();
-                });
-            };
-        };
-    };
-};
-
-exports.onDisconnect = function onDisconnect (cp) {
+exports.mkOnExit = function mkOnExit(mkChildExit) {
+  return function onExit(cp) {
     return function (cb) {
-        return function () {
-            cp.on('disconnect', cb);
-        };
+      return function () {
+        cp.on("exit", function (code, signal) {
+          cb(mkChildExit(code)(signal))();
+        });
+      };
     };
+  };
 };
 
-exports.mkOnMessage = function mkOnMessage (nothing) {
-    return function (just) {
-        return function onMessage (cp) {
-            return function (cb) {
-                return function () {
-                    cp.on('message', function (mess, sendHandle) {
-                        cb(mess, sendHandle ? just(sendHandle) : nothing)();
-                    });
-                };
-            };
-        };
-    };
-};
-
-exports.onError = function onError (cp) {
+exports.mkOnClose = function mkOnClose(mkChildExit) {
+  return function onClose(cp) {
     return function (cb) {
-        return function () {
-            cp.on('error', function (err) {
-                cb(err)();
-            });
-        };
+      return function () {
+        cp.on("close", function (code, signal) {
+          cb(mkChildExit(code)(signal))();
+        });
+      };
     };
+  };
+};
+
+exports.onDisconnect = function onDisconnect(cp) {
+  return function (cb) {
+    return function () {
+      cp.on("disconnect", cb);
+    };
+  };
+};
+
+exports.mkOnMessage = function mkOnMessage(nothing) {
+  return function (just) {
+    return function onMessage(cp) {
+      return function (cb) {
+        return function () {
+          cp.on("message", function (mess, sendHandle) {
+            cb(mess, sendHandle ? just(sendHandle) : nothing)();
+          });
+        };
+      };
+    };
+  };
+};
+
+exports.onError = function onError(cp) {
+  return function (cb) {
+    return function () {
+      cp.on("error", function (err) {
+        cb(err)();
+      });
+    };
+  };
 };
 
 exports.undefined = undefined;
