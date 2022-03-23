01e3ade55d0b12ba0ba26bf174fe0f3b9646b09c
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2022-03-23 14:36:33 +0000

    Update to PureScript v0.15.0 (#31)
    * ESM conversion
    
    * Arrow transformations, fix dynamic imports
    
    * Fix missing export
    
    * Fix fork
    
    * Fix process export
    
    * Removed '"use strict";' in FFI files
    
    * Update to CI to use 'unstable' purescript
    
    * Update Bower dependencies to master or main
    
    * Update pulp to 16.0.0-0
    
    * Update psa to 0.8.2
    
    * Update Bower dependencies to master or main
    
    * Update eslintrc
    
    * Fix eslint errors
    
    * Update changelog
    
    * Update ci.yml to v2
    
    Co-authored-by: Nicholas Wolverson <nicholas.wolverson@gmail.com>

--- src/Node/ChildProcess.js
+++ src/Node/ChildProcess.js
@@ -1,140 +1,96 @@
-"use strict";
-
 /* eslint-env node*/
 
-exports.unsafeFromNullable = function unsafeFromNullable(msg) {
-  return function (x) {
+import { spawn, exec, execFile, execSync, execFileSync, fork as cp_fork } from "child_process";
+
+export function unsafeFromNullable(msg) {
+  return x => {
     if (x === null) throw new Error(msg);
     return x;
   };
+}
+
+export function spawnImpl(command) {
+  return args => opts => () => spawn(command, args, opts);
+}
+
+export function execImpl(command) {
+  return opts => callback => () => exec(
+    command,
+    opts,
+    (err, stdout, stderr) => {
+      callback(err)(stdout)(stderr)();
+    }
+  );
+}
+
+export const execFileImpl = function execImpl(command) {
+  return args => opts => callback => () => execFile(
+    command,
+    args,
+    opts,
+    (err, stdout, stderr) => {
+      callback(err)(stdout)(stderr)();
+    }
+  );
 };
 
-exports.spawnImpl = function spawnImpl(command) {
-  return function (args) {
-    return function (opts) {
-      return function () {
-        return require("child_process").spawn(command, args, opts);
-      };
-    };
-  };
-};
+export function execSyncImpl(command) {
+  return opts => () => execSync(command, opts);
+}
 
-exports.execImpl = function execImpl(command) {
-  return function (opts) {
-    return function (callback) {
-      return function () {
-        return require("child_process").exec(
-          command,
-          opts,
-          function (err, stdout, stderr) {
-            callback(err)(stdout)(stderr)();
-          }
-        );
-      };
-    };
-  };
-};
+export function execFileSyncImpl(command) {
+  return args => opts => () => execFileSync(command, args, opts);
+}
 
-exports.execFileImpl = function execImpl(command) {
-  return function (args) {
-    return function (opts) {
-      return function (callback) {
-        return function () {
-          return require("child_process").execFile(
-            command,
-            args,
-            opts,
-            function (err, stdout, stderr) {
-              callback(err)(stdout)(stderr)();
-            }
-          );
-        };
-      };
-    };
-  };
-};
+export function fork(cmd) {
+  return args => () => cp_fork(cmd, args);
+}
 
-exports.execSyncImpl = function execSyncImpl(command) {
-  return function (opts) {
-    return function () {
-      return require("child_process").execSync(command, opts);
-    };
-  };
-};
-
-exports.execFileSyncImpl = function execFileSyncImpl(command) {
-  return function (args) {
-    return function (opts) {
-      return function () {
-        return require("child_process").execFileSync(command, args, opts);
-      };
-    };
-  };
-};
-
-exports.fork = function fork(cmd) {
-  return function (args) {
-    return function () {
-      return require("child_process").fork(cmd, args);
-    };
-  };
-};
-
-exports.mkOnExit = function mkOnExit(mkChildExit) {
+export function mkOnExit(mkChildExit) {
   return function onExit(cp) {
-    return function (cb) {
-      return function () {
-        cp.on("exit", function (code, signal) {
-          cb(mkChildExit(code)(signal))();
-        });
-      };
-    };
-  };
-};
-
-exports.mkOnClose = function mkOnClose(mkChildExit) {
-  return function onClose(cp) {
-    return function (cb) {
-      return function () {
-        cp.on("close", function (code, signal) {
-          cb(mkChildExit(code)(signal))();
-        });
-      };
-    };
-  };
-};
-
-exports.onDisconnect = function onDisconnect(cp) {
-  return function (cb) {
-    return function () {
-      cp.on("disconnect", cb);
-    };
-  };
-};
-
-exports.mkOnMessage = function mkOnMessage(nothing) {
-  return function (just) {
-    return function onMessage(cp) {
-      return function (cb) {
-        return function () {
-          cp.on("message", function (mess, sendHandle) {
-            cb(mess, sendHandle ? just(sendHandle) : nothing)();
-          });
-        };
-      };
-    };
-  };
-};
-
-exports.onError = function onError(cp) {
-  return function (cb) {
-    return function () {
-      cp.on("error", function (err) {
-        cb(err)();
+    return cb => () => {
+      cp.on("exit", (code, signal) => {
+        cb(mkChildExit(code)(signal))();
       });
     };
   };
-};
+}
 
-exports.undefined = undefined;
-exports.process = process;
+export function mkOnClose(mkChildExit) {
+  return function onClose(cp) {
+    return cb => () => {
+      cp.on("close", (code, signal) => {
+        cb(mkChildExit(code)(signal))();
+      });
+    };
+  };
+}
+
+export function onDisconnect(cp) {
+  return cb => () => {
+    cp.on("disconnect", cb);
+  };
+}
+
+export function mkOnMessage(nothing) {
+  return just => (function onMessage(cp) {
+    return cb => () => {
+      cp.on("message", (mess, sendHandle) => {
+        cb(mess, sendHandle ? just(sendHandle) : nothing)();
+      });
+    };
+  });
+}
+
+export function onError(cp) {
+  return cb => () => {
+    cp.on("error", err => {
+      cb(err)();
+    });
+  };
+}
+
+const _undefined = undefined;
+export { _undefined as undefined };
+import process from "process";
+export { process };
