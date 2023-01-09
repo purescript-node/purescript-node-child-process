ae42e31094859386fe5b53f6fc417c3ec16da508
Walker <walker@praiseweb.com.br>
2023-01-09 11:42:25 -0300

    Add logTest on Aff

--- test/Node/ChildProcess/Aff.purs
+++ test/Node/ChildProcess/Aff.purs
@@ -5,23 +5,25 @@ import Data.Functor ((<$>))
 import Control.Bind ((=<<))
 import Data.Tuple (fst)
 import Effect.Aff (Aff)
-import Effect.Console (log)
 import Effect.Class (liftEffect)
 import Node.ChildProcess (stdout, defaultSpawnOptions)
 import Node.ChildProcess.Aff (spawn)
 import Node.Buffer (toString, concat)
 import Node.Stream.Aff (readAll)
 import Node.Encoding (Encoding (UTF8))
+import Test.Internal (logTest)
 
 test :: Aff Unit
 test = do
-  liftEffect $ log "running Test.Node.ChildProcess.Aff test"
+  let log' = logTest "Aff test"
+  liftEffect $ log' "running Test.Node.ChildProcess.Aff test"
   spawnLs
 
 spawnLs :: Aff Unit
 spawnLs = do
-  liftEffect $ log "spawns processes ok"
+  let log' = logTest "Aff spawnLs"
+  liftEffect $ log' "spawns processes ok"
   ls <- spawn "ls" ["-la"] defaultSpawnOptions
   xs <- fst <$> (readAll $ stdout ls)
   output <- liftEffect $ concat xs
-  liftEffect $ log =<< toString UTF8 output
+  liftEffect $ log' =<< toString UTF8 output
