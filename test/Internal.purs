32599aab4c4f7169cc5757ee49cee5a84c88e590
Walker <walker@praiseweb.com.br>
2023-01-09 11:33:13 -0300

    Improve test logging to organize async computations

--- test/Internal.purs
+++ test/Internal.purs
@@ -1,10 +1,14 @@
-module Test.Internal (makeNonCancelerAff) where 
+module Test.Internal (makeNonCancelerAff, logTest) where 
 
-import Prelude (Unit, (*>), pure)
+import Prelude (Unit, ($), (*>), (<>), pure)
 import Data.Either (Either)
 import Effect (Effect)
 import Effect.Aff (Aff, makeAff, nonCanceler)
+import Effect.Console (log)
 import Effect.Exception (Error)
 
 makeNonCancelerAff :: forall a b. ((Either Error a -> Effect Unit) -> Effect b) -> Aff a
 makeNonCancelerAff handler = makeAff (\cb -> handler cb *> pure nonCanceler)
+
+logTest :: String -> String -> Effect Unit
+logTest testName s = log $ "[" <> testName <> "] " <> s
--- test/Node/ChildProcess.purs
+++ test/Node/ChildProcess.purs
@@ -7,7 +7,6 @@ import Data.Posix.Signal (Signal(..))
 import Data.Either (Either(Left, Right))
 import Control.Parallel (parSequence_)
 import Effect (Effect)
-import Effect.Console (log)
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
 import Effect.Exception (Error, error)
@@ -16,11 +15,12 @@ import Node.ChildProcess (Exit(..), defaultExecOptions, exec, defaultExecSyncOpt
 import Node.Encoding (Encoding(UTF8))
 import Node.Encoding as NE
 import Node.Stream (onData)
-import Test.Internal (makeNonCancelerAff)
+import Test.Internal (makeNonCancelerAff, logTest)
 
 test :: Aff Unit
 test = do
-    liftEffect $ log "running Test.Node.ChildProcess"
+    let log' = logTest "test"
+    liftEffect $ log' "running Test.Node.ChildProcess"
     parSequence_ [
       makeNonCancelerAff spawnLs,
       makeNonCancelerAff nonExistentExecutable, 
@@ -31,63 +31,69 @@ test = do
 
 spawnLs :: forall a. (Either a Unit -> Effect Unit) -> Effect Unit
 spawnLs cb = do
-  log "spawns processes ok"
+  let log' = logTest "spawnLs"
+  log' "spawns processes ok"
   ls <- spawn "ls" ["-la"] defaultSpawnOptions
   onExit ls \exit -> do
-     log $ "ls exited: " <> show exit
+     log' $ "ls exited: " <> show exit
      cb $ Right unit
-  onData (stdout ls) (Buffer.toString UTF8 >=> log)
+  onData (stdout ls) (Buffer.toString UTF8 >=> log')
 
 nonExistentExecutable :: (Either Error Unit -> Effect Unit) -> Effect Unit
 nonExistentExecutable cb = do
-  log "emits an error if executable does not exist"
+  let log' = logTest "nonExistentExecutable"
+  log' "emits an error if executable does not exist"
   ch <- spawn "this-does-not-exist" [] defaultSpawnOptions
-  onError ch (\err -> log err.code *> cb (Left $ error err.code))
-  log "nonexistent executable: all good."
+  onError ch (\err -> log' err.code *> cb (Left $ error err.code))
+  log' "nonexistent executable: all good."
   cb (Right unit)
 
 spawnLsErr :: (Either Error Unit -> Effect Unit) -> Effect Unit
 spawnLsErr cb = do
-    log "doesn't perform effects too early"
+    let log' = logTest "spawnLsErr"
+    log' "doesn't perform effects too early"
     spawn "ls" ["-la"] defaultSpawnOptions >>= \ls -> do
       let _ = kill SIGTERM ls
       onExit ls \exit ->
         case exit of
           Normally 0 -> do
-            log "All good!"
+            log' "All good!"
             cb $ Right unit
           _ -> do
             let e = error ("Bad exit: expected `Normally 0`, got: " <> show exit)
-            log $ show e
+            log' $ show e
             cb $ Left e
 
 spawnLsKills :: (Either Error Unit -> Effect Unit) -> Effect Unit
 spawnLsKills cb = do
-    log "kills processes"
+    let log' = logTest "spawnLsKills"
+    log' "kills processes"
     spawn "ls" ["-la"] defaultSpawnOptions >>= \ls -> do
       _ <- kill SIGTERM ls
       onExit ls \exit ->
         case exit of
           BySignal SIGTERM -> do
-            log "All good!"
+            log' "All good!"
             cb $ Right unit
           _ -> do
             let e = error ("Bad exit: expected `BySignal SIGTERM`, got: " <> show exit)
-            log $ show e
+            log' $ show e
             cb $ Left e
 
 execLs :: (Either Error Unit -> Effect Unit) -> Effect Unit
 execLs cb = do
-    log "exec"
+    let log' = logTest "execLs"
+    log' "exec"
     -- returned ChildProcess is ignored here
     _ <- exec "ls >&2" defaultExecOptions \r -> do
-      log "redirected to stderr:"
-      (Buffer.toString UTF8 r.stderr >>= log)
+      log' "redirected to stderr:"
+      (Buffer.toString UTF8 r.stderr >>= log')
       cb $ Right unit
     pure unit
 
 execSyncEcho :: String -> Effect Unit
 execSyncEcho str = do
+  let log' = logTest "execSyncEcho"
   resBuf <- execSync "cat" (defaultExecSyncOptions {input = Just str})
   res <- Buffer.toString NE.UTF8 resBuf
-  log res
+  log' res
