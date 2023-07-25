028fd2a22e6de0664ee84e92eb129e64d272f0f6
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-07-25 10:31:12 -0700

    Fix linting issue

--- src/Node/ChildProcess/Types.js
+++ src/Node/ChildProcess/Types.js
@@ -3,4 +3,4 @@ export const fromKillSignalImpl = (left, right, sig) => {
   if (ty === "number") return right(sig | 0);
   if (ty === "string") return left(sig);
   throw new Error("Impossible. Got kill signal that was neither int nor string: " + sig);
-}
+};
