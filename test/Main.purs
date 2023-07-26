d17188032fdef69345cb73547e7559b8044c5a84
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-07-26 14:13:27 -0700

    Update tests

--- test/Main.purs
+++ test/Main.purs
@@ -10,16 +10,15 @@ import Effect (Effect)
 import Effect.Aff (Aff, effectCanceler, launchAff_, makeAff, nonCanceler)
 import Effect.Class (liftEffect)
 import Effect.Class.Console (log)
-import Effect.Exception (throwException)
+import Effect.Exception (throw, throwException)
 import Node.Buffer as Buffer
-import Node.ChildProcess (exec', execSync', kill, spawn, stdin, stdout)
+import Node.ChildProcess (exec', execSync', kill, spawn, stdin)
 import Node.ChildProcess as CP
+import Node.ChildProcess.Aff (waitSpawned)
 import Node.ChildProcess.Types (Exit(..), fromKillSignal)
 import Node.Encoding (Encoding(..))
 import Node.Encoding as NE
-import Node.Errors.SystemError (code)
-import Node.EventEmitter (EventHandle, on_, once, once_)
-import Node.Stream (dataH)
+import Node.EventEmitter (EventHandle, once, once_)
 import Node.Stream as Stream
 import Unsafe.Coerce (unsafeCoerce)
 
@@ -64,17 +63,24 @@ spawnLs :: Aff Unit
 spawnLs = do
   log "\nspawns processes ok"
   ls <- liftEffect $ spawn "ls" [ "-la" ]
-  liftEffect $ (stdout ls) # on_ dataH (Buffer.toString UTF8 >=> log)
-  exit <- until ls CP.exitH \complete -> \exit -> complete exit
-  log $ "ls exited: " <> show exit
+  res <- waitSpawned ls
+  case res of
+    Right pid -> log $ "ls successfully spawned with PID: " <> show pid
+    Left err -> liftEffect $ throwException $ unsafeCoerce err
+  exit <- until ls CP.closeH \complete -> \exit -> complete exit
+  case exit of
+    Normally 0 -> log $ "ls exited with 0"
+    Normally i -> liftEffect $ throw $ "ls had non-zero exit: " <> show i
+    BySignal sig -> liftEffect $ throw $ "ls exited with sig: " <> show sig
 
 nonExistentExecutable :: Aff Unit
 nonExistentExecutable = do
   log "\nemits an error if executable does not exist"
   ch <- liftEffect $ spawn "this-does-not-exist" []
-  err <- until ch CP.errorH \complete -> \err -> complete err
-  log (code err)
-  log "nonexistent executable: all good."
+  res <- waitSpawned ch
+  case res of
+    Left _ -> log "nonexistent executable: all good."
+    Right pid -> liftEffect $ throw $ "nonexistent executable started with PID: " <> show pid
 
 noEffectsTooEarly :: Aff Unit
 noEffectsTooEarly = do
@@ -85,8 +91,8 @@ noEffectsTooEarly = do
   case exit of
     Normally 0 ->
       log "All good!"
-    _ -> do
-      log ("Bad exit: expected `Normally 0`, got: " <> show exit)
+    _ ->
+      liftEffect $ throw $ "Bad exit: expected `Normally 0`, got: " <> show exit
 
 killsProcess :: Aff Unit
 killsProcess = do
@@ -98,7 +104,7 @@ killsProcess = do
     BySignal s | Just SIGTERM <- Signal.fromString =<< (hush $ fromKillSignal s) ->
       log "All good!"
     _ -> do
-      log ("Bad exit: expected `BySignal SIGTERM`, got: " <> show exit)
+      liftEffect $ throw $ "Bad exit: expected `BySignal SIGTERM`, got: " <> show exit
 
 execLs :: Aff Unit
 execLs = do
@@ -107,7 +113,13 @@ execLs = do
     -- returned ChildProcess is ignored here
     void $ exec' "ls >&2" identity (done <<< Right)
     pure nonCanceler
-  log "redirected to stderr:" *> (liftEffect $ Buffer.toString UTF8 r.stderr >>= log)
+  stdout' <- liftEffect $ Buffer.toString UTF8 r.stdout
+  stderr' <- liftEffect $ Buffer.toString UTF8 r.stderr
+  when (stdout' /= "") do
+    liftEffect $ throw $ "stdout should be redirected to stderr but had content: " <> show stdout'
+  when (stderr' == "") do
+    liftEffect $ throw $ "stderr should have content but was empty"
+  log "stdout was successfully redirected to stderr"
 
 execSyncEcho :: String -> Aff Unit
 execSyncEcho str = liftEffect do
@@ -115,4 +127,6 @@ execSyncEcho str = liftEffect do
   buf <- Buffer.fromString str UTF8
   resBuf <- execSync' "cat" (_ { input = Just buf })
   res <- Buffer.toString NE.UTF8 resBuf
-  log res
+  when (str /= res) do
+    throw $ "cat did not output its input. \nGot: " <> show res <> "\nExpected: " <> show str
+  log "cat successfully re-outputted its input"
