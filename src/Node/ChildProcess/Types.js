3cac254646fef06b9d9624c10f1e152f49cb03b9
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-07-26 14:07:06 -0700

    Fix `fromKillSignalImpl` FFI arg order

--- src/Node/ChildProcess/Types.js
+++ src/Node/ChildProcess/Types.js
@@ -1,8 +1,8 @@
 export const showKillSignal = (ks) => ks + "";
 export const showShell = (shell) => shell + "";
-export const fromKillSignalImpl = (left, right, sig) => {
+export const fromKillSignalImpl = (fromInt, fromStr, sig) => {
   const ty = typeof sig;
-  if (ty === "number") return right(sig | 0);
-  if (ty === "string") return left(sig);
+  if (ty === "number") return fromInt(sig | 0);
+  if (ty === "string") return fromStr(sig);
   throw new Error("Impossible. Got kill signal that was neither int nor string: " + sig);
 };
